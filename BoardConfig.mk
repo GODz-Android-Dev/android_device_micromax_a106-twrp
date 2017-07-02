a1fb09c4f1507dbad99673b66c5764e134fdee4e
Dozerators <paranyuk.ura@gmail.com>
2017-07-03 02:37:57 +0300

    TWRP

---
+++ BoardConfig.mk
@@ -0,0 +1,86 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Product-specific compile-time definitions.
+#
+
+LOCAL_PATH := device/Lenovo/A536
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6582
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT:= cortex-a7
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+
+TARGET_CPU_SMP := true
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Kernel
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100
+TARGET_RECOVERY_PREBUILT_KERNEL := $(LOCAL_PATH)/recovery/kernel
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4:= true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00600000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15040448
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 956964608
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147467264 (2147483648-16384)
+BOARD_CACHEIMAGE_PARTITION_SIZE := 134217728
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+#Recovery
+TW_THEME := portrait_hdpi
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_RECOVERY_SWIPE := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_USE_EXTERNAL_STORAGE := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_INCLUDE_L_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p8"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone5/temp"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := "/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness"
+TW_INCLUDE_NTFS_3G := true
+TW_DEFAULT_LANGUAGE:= ru
+TW_INCLUDE_FB2PNG := true
+TWRP_INCLUDE_LOGCAT := true
+TW_INCLUDE_JPEG := true
+
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/tools/bootimg.mk
---
+++ README.md
@@ -0,0 +1,5 @@
+TeamWin Recovery Project
+
+Device configuration for Lenovo A5000 (sprout)
+
+
